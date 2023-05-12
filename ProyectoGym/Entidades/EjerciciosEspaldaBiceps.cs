256614b4edbe6cd2643c47845c0cabdb56de76b2
Godofasto <hiddennymeria@gmail.com>
2023-05-12 11:19:37 +0200

    11:20 12/05/2023

--- ProyectoGym/Entidades/EjerciciosEspaldaBiceps.cs
+++ ProyectoGym/Entidades/EjerciciosEspaldaBiceps.cs
@@ -7,6 +7,7 @@
         public string Repeticiones { get; set; }
         public string urlImagen { get; set; }
         public string ulrVideo { get; set; }
+        public string Tipo { get; set; }
 
     }
 }
